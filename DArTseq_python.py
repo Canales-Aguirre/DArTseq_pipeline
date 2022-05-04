157bbdc7c7204ae15a5dc6a79d1a101c5cb4ce43
Sander de Backer <91179859+sanderdebacker@users.noreply.github.com>
2022-05-04 18:10:55 +0200

    Update DArTseq_python.py
    sys.exit('--extension argument is missing') doesn't work still

--- DArTseq_python.py
+++ DArTseq_python.py
@@ -1,6 +1,6 @@
 #/bin/python3
 
-import os, argparse, subprocess
+import os, sys, argparse, subprocess
 
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser(description = 'Process raw fastq reads into VCF files.')
@@ -11,8 +11,54 @@ parser.add_argument('--reference', '--r', type = str, help = 'Reference fasta fi
 # Parse arguments to a dictionary
 args = vars(parser.parse_args())
 
+# DEFINE FUNCTIONS
+# Mapping with BWA MEM
+def mapping(fastq):
+    if args['extension'] == None:
+        sys.exit('--extension argument is missing')
+    elif args['reference'] == None:
+        sys.exit('--reference argument is missing')
+    else:
+        subprocess.run(["bwa", "mem", args['reference'], fastq, ">", "${fastq%.fastq.gz}.sam"])
+    
+# ANALYSIS        
+# Run function mapping()
 for file in os.listdir():
-    if file.endswith(args['extension']):
+    if file.endswith(str(args['extension'])):
         print(file)
-        print(len(file))
-        subprocess.run(["bwa", "mem", args['reference'], file])
+        mapping(file)
+      
+#/bin/python3
+
+import os, sys, argparse, subprocess
+
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser(description = 'Process raw fastq reads into VCF files.')
+
+parser.add_argument('--extension', '--e', type = str, help = 'Extension of files')
+parser.add_argument('--reference', '--r', type = str, help = 'Reference fasta file')
+
+# Parse arguments to a dictionary
+args = vars(parser.parse_args())
+
+# DEFINE FUNCTIONS
+# Mapping with BWA MEM
+def mapping(qz):
+    fastq_gz = os.path.splitext(qz)
+    fastq = fastq_gz[0]
+    name_fastq = os.path.splitext(fastq)
+    name = name_fastq[0]
+    print(name)
+    if args['extension'] == None:
+        sys.exit('--extension argument is missing')
+    elif args['reference'] == None:
+        sys.exit('--reference argument is missing')
+    else:
+        subprocess.run(["bwa", "mem", args['reference'], gz, "-o", basename[0]])
+    
+# ANALYSIS        
+# Run function mapping()
+for file in os.listdir():
+    if file.endswith(str(args['extension'])):
+        print(file)
+        mapping(file)
