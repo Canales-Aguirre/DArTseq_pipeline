b6855e582f741a6f4338c381b1f8cbfc2df952bc
Sander de Backer <91179859+sanderdebacker@users.noreply.github.com>
2022-05-05 15:59:54 +0200

    Update DArTseq_python.py

--- DArTseq_python.py
+++ DArTseq_python.py
@@ -2,63 +2,89 @@
 
 import os, sys, argparse, subprocess
 
+# ARGUMENTS
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser(description = 'Process raw fastq reads into VCF files.')
 
-parser.add_argument('--extension', '--e', type = str, help = 'Extension of files')
-parser.add_argument('--reference', '--r', type = str, help = 'Reference fasta file')
+# Required arguments
+parser.add_argument('--reference', '--r', type = str, help = 'Reference fasta file.')
+parser.add_argument('--picard', '--p', type = str, help = 'Path to picard.jar.')
+parser.add_argument('--gatk4', '--g', type = str, help = 'Path to GATK4 installation')
+
+# Optional arguments
+parser.add_argument('--threads', '--t', default = 1, type = int, help = 'number of threads to be used.')
+parser.add_argument('--notrimming', action = 'store_true', help = 'skip the GBprocesS trimming step.')
+parser.add_argument('--nomapping', action = 'store_true', help = 'skip the BWA MEM mapping step.')
+parser.add_argument('--noaddreadgroup', action = 'store_true', help = 'skip the AddOrReplaceReadGroups (picard) step.')
+parser.add_argument('--novariantcalling', action = 'store_true', help = 'skip the FreeBayes variant calling step.')
+parser.add_argument('--novariantfilter', action = 'store_true', help = 'skip the GATK4 variant filtration step.')
 
 # Parse arguments to a dictionary
 args = vars(parser.parse_args())
 
-# DEFINE FUNCTIONS
-# Mapping with BWA MEM
-def mapping(fastq):
-    if args['extension'] == None:
-        sys.exit('--extension argument is missing')
-    elif args['reference'] == None:
-        sys.exit('--reference argument is missing')
-    else:
-        subprocess.run(["bwa", "mem", args['reference'], fastq, ">", "${fastq%.fastq.gz}.sam"])
-    
-# ANALYSIS        
-# Run function mapping()
-for file in os.listdir():
-    if file.endswith(str(args['extension'])):
-        print(file)
-        mapping(file)
-      
-#/bin/python3
-
-import os, sys, argparse, subprocess
-
-# Create an ArgumentParser object
-parser = argparse.ArgumentParser(description = 'Process raw fastq reads into VCF files.')
-
-parser.add_argument('--extension', '--e', type = str, help = 'Extension of files')
-parser.add_argument('--reference', '--r', type = str, help = 'Reference fasta file')
-
-# Parse arguments to a dictionary
-args = vars(parser.parse_args())
+# Check for required arguments
+if args['reference'] == None:
+    print("Oops, somthing went wrong...")
+    sys.exit('Argument missing: --reference argument is required.')
+elif args['noaddreadgroup'] is False and args['picard'] == None:
+    print("Oops, somthing went wrong...")
+    sys.exit('Argument missing: --picard argument is required for the AddOrReplaceReadGroup step. Otherwise use --noaddreadgroup argument.')
+elif args['novariantfilter'] is False and args['gatk4'] == None:
+    print("Oops, somthing went wrong...")
+    sys.exit('Argument missing: --gatk4 argument is required for the variant filtration step. Otherwise use --novariantfilter argument.')
 
 # DEFINE FUNCTIONS
-# Mapping with BWA MEM
-def mapping(qz):
-    fastq_gz = os.path.splitext(qz)
-    fastq = fastq_gz[0]
-    name_fastq = os.path.splitext(fastq)
-    name = name_fastq[0]
-    print(name)
-    if args['extension'] == None:
-        sys.exit('--extension argument is missing')
-    elif args['reference'] == None:
-        sys.exit('--reference argument is missing')
-    else:
-        subprocess.run(["bwa", "mem", args['reference'], gz, "-o", basename[0]])
+# Trimming with GBprocesS
+def trimming(raw):
+    """Trimming of the raw fastq files with GBprocesS (cutadapt)."""
     
+
+# Mapping with BWA MEM
+def mapping(gz):
+    """Mapping of raw fastq.gz file on reference sequence."""
+    name = (os.path.splitext((os.path.splitext(gz))[0]))[0]
+    ref = os.path.basename(args['reference'])
+    print("Mapping " + gz + " on " + ref + "... Writing " + (name + ".sam"))
+    subprocess.run(["bwa", "mem", "-t", str(args['threads']), args['reference'], gz, "-o", name + ".sam"])
+
+# Reorganising SAM with AddOrReplaceReadGroups (picard)
+def add_rg(sam):
+    """Reorganising SAM file with AddOrReplaceReadGroups (picard) and writing BAM file."""
+    name = (os.path.splitext(sam))[0]
+    print("Reorganising " + sam + "... Writing " + name + ".recalibrated.bam")
+    subprocess.run(["java", "-jar", args['picard'], "AddOrReplaceReadGroups", "-I", sam, "-O", name + ".recalibrated.bam", "--CREATE_INDEX", "-SO", "coordinate", "-RGLB", name, "-RGPL", "illumina", "-RGPU", "run", "-RGSM", name, "-RGID", name])
+
+# Variant calling with FreeBayes
+def varcal_freebayes(bam):
+    """Variant calling on the BAM file with FreeBayes."""
+    name = (os.path.splitext(bam))[0]
+    print("Variant calling on " + bam + "... Writing VCF file")
+    subprocess.run(["freebayes", "-f", args['reference'], "--genotype-qualities", "--strict-vcf", "--vcf", name + ".freebayes.vcf", bam])
+    
+# SNP filtering with GATK4 SelectVariants
+def filter_vcf(fb_vcf):
+    """Filter SNPs from the FreeBayes VCF with GATK4 SelectVariants."""
+    name = (os.path.splitext(fb_vcf))[0]
+    print("Filtering SNPs from " + fb_vcf + " and writing new VCF file...")
+    subprocess.run([args['gatk4'], "SelectVariants", "-R", args['reference'], "-V", fb_vcf, "--select-type-to-include", "SNP", "-O", name + ".filtered.vcf"])
+
 # ANALYSIS        
-# Run function mapping()
 for file in os.listdir():
-    if file.endswith(str(args['extension'])):
-        print(file)
-        mapping(file)
+    if file.endswith(".GBprocesS.FASTQ.gz"):
+        if args['nomapping'] is False:
+            mapping(file)
+       
+for file in os.listdir():
+    if file.endswith(".sam"):
+        if args['noaddreadgroup'] is False:
+            add_rg(file)
+        
+for file in os.listdir():
+    if file.endswith(".bam"):
+        if args['novariantcalling'] is False:
+            varcal_freebayes(file)
+            
+for file in os.listdir():
+    if file.endswith(".freebayes.vcf"):
+        if args['novariantfilter'] is False:
+            filter_vcf(file)
