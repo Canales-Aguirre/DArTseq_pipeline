284d2601197185391f06fb61a05424fae7662484
Sander de Backer <91179859+sanderdebacker@users.noreply.github.com>
2022-07-01 11:40:56 +0200

    Update DArTseq_blastn.py

--- DArTseq_blastn.py
+++ DArTseq_blastn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os, sys, argparse, subprocess, gzip
+from Bio import SeqIO
 from datetime import datetime
 
 # ARGUMENTS
@@ -24,8 +25,30 @@ def print_date():
     sys.stderr.write('********************\n\n')
     return
 
+# Open FASTQ(.gz) and convert to fasta
+def fastq_to_fasta(fastq_file):
+    if '.gz' in fastq_file:
+	    print 'Detected a .gz file.'
+        name = (os.path.splitext((os.path.splitext(fastq_file))[0]))[0]
+	    fastq = gzip.open(fastq_file, 'rb')
+    else:
+        name = (os.path.splitext(fastq_file))[0]
+	    fastq = open(fastq_file, 'r')
+    subprocess.run(["sed", "-n", "'1~4s/^@/>/p;2~4p'", fastq, ">", name + ".fasta"])
+    
 # Blast remotely against NCBI database
-def DArT_blast(file)
-    """Blast query FASTQ.gz file remotely against NCBI nucleotide database. Each sequence is blasted seperately and merged at the end."""
-    with gzip.open(file,'r') as DArT_file:
-        for line in DArT_file
+def DArTseq_blast(fastq_file)
+    """Blast newly converted fasta file remotely against NCBI nucleotide database. Each sequence is blasted seperately and merged at the end."""
+    if '.gz' in fastq_file:
+        name = (os.path.splitext((os.path.splitext(fastq_file))[0]))[0] 
+        fasta_file = name + ".fasta"
+    else:
+        name = (os.path.splitext(fastq_file))[0] + ".fasta"
+        fasta_file = name + ".fasta"
+    for record in SeqIO.parse(name, "fasta"):
+        subprocess.run(["blastn", "-db", "nt", "-query", fasta_file, "-num_alignments", "10", "-evalue", "0.01", "-word_size", "50", "-remote", ">", name + ".blast.out"])
+
+# ANALYSIS
+
+fastq_to_fasta(args['sample'])
+DArTseq_blast(args['sample'])
